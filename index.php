4a6c6aea300fdbebb94ee9bba0b564b472eb3209
Mark Goldthorp <code@koalatea-software.com>
2020-08-23 13:14:06 +0100

    Now handles chapter requests

--- index.php
+++ index.php
@@ -1,11 +1,69 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/components/logging/errorHandler.php';
-error_log("Hello");
+
+// some default values
 $titleTag = "Rose Goldthorp";
 $chapter = "home";
 $pagePath = $_SERVER['DOCUMENT_ROOT'] . "/chapters" . DIRECTORY_SEPARATOR . $chapter;
-$msg = "'gday";
+$msg = "Defaulting to Home";
 $meta = "Some meta text";
+
+/*
+ * The following depends on the rules in the .htaccess file, i.e
+ * RewriteRule ^.*$ /index.php?%{QUERY_STRING}
+ *
+ * All the chapters will be in the /chapters folder
+ * Each chapter has a directory with the right name,and containing a contents.php
+ */
+$askedFor = $_SERVER["REQUEST_URI"]; // this is what was actually asked-for
+$majorParts = explode('?', $askedFor);
+$pathElements = explode('/', $majorParts[0]);
+error_log("Path Elements:" . print_r($pathElements, true));
+
+$chapter = isset($pathElements[1]) ? $pathElements[1] : "";
+$section = isset($pathElements[2]) ? $pathElements[2] : "";
+$subSection = isset($pathElements[3]) ? $pathElements[3] : "";
+
+if (strlen($chapter) < 1) {
+    error_log("No chapter has been requested");
+    $chapter = "home";
+    $pagePath = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "chapters" . DIRECTORY_SEPARATOR . $chapter;
+    $msg = "Home";
+} else {
+    $chapter = strtolower($chapter);
+    $pagePath = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "chapters" . DIRECTORY_SEPARATOR . $chapter;
+
+    if (!is_dir($pagePath)) {
+        $msg = "Unknown-Chapter(" . $chapter . ")";
+        header('Location: /?' . $msg);
+        exit();
+    }
+
+    if (!file_exists($pagePath . DIRECTORY_SEPARATOR . "contents.php")) {
+        $msg = "Unknown-Chapter(" . $chapter . ")";
+        header('Location: /?' . $msg);
+        exit();
+    }
+
+    if (file_exists($pagePath . DIRECTORY_SEPARATOR . "meta.txt")) {
+        $meta = file_get_contents($pagePath . DIRECTORY_SEPARATOR . "meta.txt");
+    }
+
+    $titleTag = ucfirst($chapter);
+
+    // note: we will only bother looking at the section if the chapter was good
+    if (strlen($section) > 1) {
+        if (!file_exists($pagePath . DIRECTORY_SEPARATOR . "section.php")) {
+            $msg = "Unknown-Section(" . $section . ")";
+            header('Location: /' . $chapter . '?' . $msg);
+            exit();
+        }
+
+        $titleTag = ucfirst($section);
+    } // ends what to do if section is good
+}
+// if you have got to this point, then the elements of the path are nice
+// note: the $_GET is populated with whatever we were originally given (as if we had nothing fancy going on)
 ?>
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
@@ -24,6 +82,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/components/bodyParts/body-section-fur
 ?>
 
 <section id="page-content" class="">
+    <!--suppress SpellCheckingInspection -->
     <div id="contentCenterer" class="container-fluid align-content-center">
         <div class="container">
             <?php
