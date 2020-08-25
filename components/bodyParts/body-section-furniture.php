764bce2d6a6e73c91f8a7f6b4a110edaa921c1d3
Mark Goldthorp <code@koalatea-software.com>
2020-08-25 13:46:22 +0100

    * make the menu collapse at below medium as you were getting the hamburger menu too easily

--- components/bodyParts/body-section-furniture.php
+++ components/bodyParts/body-section-furniture.php
@@ -2,7 +2,7 @@
     <div id="banner">
         Rose Goldthorp: <span id="subTitle">Social Media and Content Marketeer</span>
     </div>
-    <nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav id="mainNav" class="navbar navbar-expand-md navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
                 aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
