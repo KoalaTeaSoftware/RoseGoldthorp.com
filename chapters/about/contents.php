5a36e8aabf6618e4d6dd28ef45894c9d80b372d3
Mark Goldthorp <code@koalatea-software.com>
2020-08-27 18:04:29 +0100

    Added a bit of javascript to allow deep-linking to tap panes (so back is more useful)

--- chapters/about/contents.php
+++ chapters/about/contents.php
@@ -48,6 +48,21 @@ $titleTag = "About Me";
             <h2>Comming Soon</h2>
         </div>
     </div>
+    <script>
+        $(document).ready(() => {
+            const url = window.location.href;
+            if (url.indexOf("#") > 0) {
+                const activeTab = url.substring(url.indexOf("#") + 1);
+                $('.nav[role="tablist"] a[href="#' + activeTab + '"]').tab('show');
+            }
+
+            $('a[role="tab"]').on("click", function () {
+                const hash = $(this).attr("href");
+                let newUrl = url.split("#")[0] + hash;
+                history.replaceState(null, null, newUrl);
+            });
+        });
+    </script>
 <?php
 
 /**
