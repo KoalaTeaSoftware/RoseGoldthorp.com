1a79b96920f19fdc522f46382be992501d1fcbd5
Mark Goldthorp <code@koalatea-software.com>
2020-09-02 13:01:39 +0100

    typos and realignment on the content marketing page

--- chapters/contentmarketing/contents.php
+++ chapters/contentmarketing/contents.php
@@ -1,42 +1,41 @@
 <h1>Content Marketing</h1>
-<h2>Why you need content marketing for your business</h2>
+<p class="question"><em>Why you need content marketing for your business</em></p>
 <p>Your content is how you reach out to and attract your audience from the internet. While copy and images attract,
-    video attracts even more; increaing traffic from search engines by 157%.
+    video attracts even more; increasing traffic from search engines by 157%.
 </p>
-<h2>We offer the main sorts of content marketing plus any customisation that you may need:</h2>
+<p class="question"><em>We offer the main sorts of content marketing plus any customisation that you may need:</em></p>
 <div class="container align-content-center">
-    <div class="row ">
-        <div class="di"
+    <div class="row" id="offerings">
         <div class="col">
-            <p>Infrastructure:</p>
-            <ol>
-                <li>Web sites;</li>
-                <li>Ecommerce;</li>
-                <li>Affiliate Program / Network, etc.</li>
-                <li>SEO</li>
-            </ol>
+            <p class="lead">Infrastructure:</p>
+            <div class="items">
+                <p>Web sites;</p>
+                <p>Ecommerce;</p>
+                <p>Affiliate Program / Network, etc.</p>
+                <p>SEO</p>
+            </div>
         </div>
         <div class="col">
-            <p>Blog:</p>
-            <ol>
-                <li>Weekly copy</li>
-                <li>Weekly images</li>
-            </ol>
+            <p class="lead">Blog:</p>
+            <div class="items">
+                <p>Weekly copy</p>
+                <p>Weekly images</p>
+            </div>
         </div>
         <div class="col col-xs-12">
-            <p>Videos:</p>
-            <ol>
-                <li>Inspirational - these can be either fictional narratives, or documentaries</li>
-                <li>Educational - 'how to' videos</li>
-                <li>Product showcasing - in a fun way</li>
-            </ol>
+            <p class="lead">Videos:</p>
+            <div class="items">
+                <p>Inspirational <small><em>these can be either fictional narratives, or documentaries</em></small></p>
+                <p>Educational <small><em>'how to' videos</em></small></p>
+                <p>Product showcasing <small><em>in a fun way</em></small></p>
+            </div>
         </div>
     </div>
-    <div class="row ">
+    <div class="row" id="picture">
         <figure class="align-content-center">
             <img src="/chapters/contentmarketing/roseDirectingSilverville.jpg" class="img-fluid img-thumbnail"
                  alt="Rose directing one of her earlier films (Silverville)">
-            <figcaption>Rose directing one of her earlier films
+            <figcaption>Rose directing one of her earlier films:
                 <!--suppress HtmlUnknownTarget -->
                 <a href="/about/films/silverville" class="newTabInd">Silverville</a>
             </figcaption>
