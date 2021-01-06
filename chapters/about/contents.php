14664d38d71ca93985e8a8d299f0b66b25457f68
Rosegoldthorp <code@koalatea-software.com>
2021-01-06 07:22:00 +0000

    new RR post, move jQuery from head to tail, remove the abortive about tab script

--- chapters/about/contents.php
+++ chapters/about/contents.php
@@ -69,22 +69,6 @@ $titleTag = "About Rose Goldthorp";
             makeFilmList($filmData); ?>
         </div>
     </div>
-    <script>
-        // make the back button operate as expected
-        $(document).ready(function () {
-            const url = window.location.href;
-            if (url.indexOf("#") > 0) {
-                const activeTab = url.substring(url.indexOf("#") + 1);
-                $('.nav[role="tablist"] a[href="#' + activeTab + '"]').tab('show');
-            }
-
-            $('a[role="tab"]').on("click", function () {
-                const hash = $(this).attr("href");
-                let newUrl = url.split("#")[0] + hash;
-                history.replaceState(null, null, newUrl);
-            });
-        })
-    </script>
 <?php
 
 /**
--- index.php
+++ index.php
@@ -69,16 +69,12 @@ $titleTag = ucfirst(str_replace('-', ' ', $chapter));
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="robots" content="noindex,nofollow">
     <?= $metaHtml ?>
     <title>Rose Goldthorp.com</title>
     <link rel="stylesheet" href="/styles.css">
     <link rel="icon" type="image/gif" sizes="16x16" href="/ass/logo@16px.gif">
-    <!--suppress SpellCheckingInspection -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossorigin="anonymous"></script>
-    <link rel="preload" as="image" href="ass/WorkingCogs.gif">
+    <link rel="icon" type="image/gif" sizes="320x320" href="/ass/logo@320px.gif">
+    <link rel="prefetch" as="image" href="/ass/WorkingCogs.gif">
 </head>
 <body class="container-fluid">
 <div id="furniture" class="row">
@@ -128,7 +124,10 @@ $titleTag = ucfirst(str_replace('-', ' ', $chapter));
         document.getElementById("<?= $chapter ?>Nav").classList.add("active");
     </script>
 </div>
-
+<!--suppress SpellCheckingInspection -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
 <!--suppress SpellCheckingInspection -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
