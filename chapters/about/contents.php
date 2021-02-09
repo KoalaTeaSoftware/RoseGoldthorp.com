ab566b157c1384b9cf3fafc775322791414ff794
Mark Goldthorp <code@koalatea-software.com>
2021-02-09 13:48:08 +0000

    * swap from 3rd to 1st person on the about page

--- chapters/about/contents.php
+++ chapters/about/contents.php
@@ -1,24 +1,20 @@
 <h1>About Rose Goldthorp</h1>
 <div class="row align-items-center mt-2">
     <div class="col">
-        <p>Miss Rose Goldthorp is the Writer / Director for Swords and Clapboards</p>
-        <p>Rose graduated from the University of Auckland, NZ, with a BA in Marketing and Communications with TV and
-            Film. She
-            has made five feature films and over 18 shorts in her spare time while at school and university.
+        <p>I am the Writer / Director for Swords and Clapboards.</p>
+        <p>I graduated from the University of Auckland, NZ, with a BA in Marketing and Communications with TV and
+            Film. I have made five feature films and over 18 shorts in my spare time while at school and university.
         </p>
     </div>
-    <div class="col-6"><img src="/chapters/about/rose.jpg" class="w-100 img-thumbnail" alt="Portrait of Rose Goldthorp">
+    <div class="col-6">
+        <img src="/chapters/about/rose.jpg" class="w-100 img-thumbnail" alt="Portrait of Rose Goldthorp">
     </div>
     <div class="col">
-        <p>Rose immigrated back to the UK, graduating in February 2020, and launched her transmedia website,
-            the-greenlands.com
-            for which (along with her soc. med. platforms) she draws, writes, uploads and records her fantasy drama
-            podcast;
-            art work; digital storyboards (of her Greenlands films); her illustrated blogs; her short films; her cartoon
-            strip;
-            affiliate “shops”; and soon, her accumulating collection of entertainment mobile app Greenlands-themed
-            “Oracles”
-            which she co-wrote and part shot.
+        <p>After graduating in February 2020, I immigrated back to the UK, and launched my transmedia website,
+            the-greenlands.com, for which (along with soc. med. platforms) I draw, write, upload and record my
+            fantasy drama podcast; art work; digital storyboards (of the Greenlands films); illustrated blogs;
+            short films; and cartoon strip; affiliate “shops”; and soon, an accumulating collection of
+            Greenlands-themed mobile entertainment apps (Oracles) which I co-wrote and part shot.
         </p>
     </div>
 </div>
