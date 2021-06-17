d0f394d6828bec9fb0a2e472ff60d51c4fdfce3e
goldt <admin@koalatea-software.com>
2021-06-17 16:18:37 +0100

    Some wording changes, and a new RR2F poster

--- chapters/period-brit-lit/contents.php
+++ chapters/period-brit-lit/contents.php
@@ -1,10 +1,10 @@
 <h1>The Period Brit. Lit. Project</h1>
 <div class="row align-items-center">
     <div class="col">
-        <p>In mid 2021, I am also launching another transmedia project:
+        <p>I have just launched another transmedia project:
             <a href="https://thedailydilettante.com/" target="_blank">The Daily Dilettante</a>. Again involving various
             social media platforms and also, eventually, its own web site, as the project's own 'mother ship'. Podcasts
-            are also being considered.
+            will start in the new year.
         </p>
     </div>
     <div class="col col-6">
