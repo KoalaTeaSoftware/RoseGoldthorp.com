a2b4827d3c374c8ae7cacd85ef69cfbbca4a83f2
goldt <admin@koalatea-software.com>
2021-07-12 18:34:21 +0100

    tweak a few words on about

--- chapters/about/contents.php
+++ chapters/about/contents.php
@@ -1,7 +1,7 @@
 <h1>About Rose Goldthorp</h1>
 <div class="row align-items-center mt-2">
     <div class="col">
-        <p>I am the Writer / Director for Swords and Clapboards.</p>
+        <p>I am the Writer / Director for Swords and Clapboards (unpaid intern).</p>
         <p>I graduated from the University of Auckland, NZ, with a BA in Marketing and Communications with TV and
             Film. I have made five feature films and over 18 shorts in my spare time while at school and university.
         </p>
@@ -11,10 +11,13 @@
     </div>
     <div class="col">
         <p>After graduating in February 2020, I immigrated back to the UK, and launched my transmedia website,
-            the-greenlands.com, for which (along with soc. med. platforms) I draw, write, upload and record my
+            <a href="https://the-greenlands.com/" target="_blank">The Greenlands</a>, for which (along with soc. med.
+            platforms) I draw, write, upload and record my
             fantasy drama podcast; art work; digital storyboards (of the Greenlands films); illustrated blogs;
-            short films; and cartoon strip; affiliate “shops”; and soon, an accumulating collection of
-            Greenlands-themed mobile entertainment apps (Oracles) which I co-wrote and part shot.
+            short films; and cartoon strips and affiliate “shops”. I have also just launched my second transmedia
+            website about the other half of my work: period Brit. lit. films. From the new year (2022) onwards, I will
+            also be starting a podcast series on this project's web site
+            <a href="https://thedailydilettante.com/" target="_blank">The Daily Dilettante</a>
         </p>
     </div>
 </div>
