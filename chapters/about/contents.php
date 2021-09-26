77363cf0d341df3b0e084af1170cf1388d6e4522
goldt <admin@koalatea-software.com>
2021-09-26 17:47:13 +0100

    change the words in the banner

--- chapters/about/contents.php
+++ chapters/about/contents.php
@@ -1,8 +1,9 @@
 <h1>About Rose Goldthorp</h1>
 <div class="row align-items-center mt-2">
     <div class="col">
-        <p>I am the Writer / Director for Swords and Clapboards (unpaid intern).</p>
-        <p>I graduated from the University of Auckland, NZ, with a BA in Marketing and Communications with TV and
+        <p>Hello people ... just a bit about me:</p>
+        <p>I recently graduated from the University of Auckland, NZ, with a BA in Marketing and Communications with TV
+            and
             Film. I have made five feature films and over 18 shorts in my spare time while at school and university.
         </p>
     </div>
@@ -10,14 +11,23 @@
         <img src="/chapters/about/rose.jpg" class="w-100 img-thumbnail" alt="Portrait of Rose Goldthorp">
     </div>
     <div class="col">
-        <p>After graduating in February 2020, I immigrated back to the UK, and launched my transmedia website,
+        <p>After graduating in 2020, I immigrated back to the UK, and launched my transmedia website,
             <a href="https://the-greenlands.com/" target="_blank">The Greenlands</a>, for which (along with soc. med.
             platforms) I draw, write, upload and record my
             fantasy drama podcast; art work; digital storyboards (of the Greenlands films); illustrated blogs;
-            short films; and cartoon strips and affiliate “shops”. I have also just launched my second transmedia
+            short films; and cartoon strips and affiliate “shops”. </p>
+        <p>I have also just launched my second transmedia
             website about the other half of my work: period Brit. lit. films. From the new year (2022) onwards, I will
             also be starting a podcast series on this project's web site
             <a href="https://thedailydilettante.com/" target="_blank">The Daily Dilettante</a>
         </p>
+    </div>
+</div>
+<div class="row align-items-center mt-2">
+    <div class="col">
+        <p>I hope to make the feature film versions of my Greenlands Cycle as animations. I also want to direct the
+            period Brit. lit. scripts I have co-written as live action films, whenever a producer can find these films
+            some investments. Meanwhile, lots of you lovely people are helping me, on a collaborative basis, to get
+            these scripts out into the world as podcasts.</p>
     </div>
 </div>
